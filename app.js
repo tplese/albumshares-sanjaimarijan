7bde0508da2d694de69a09a016230496d3ddf28d
Tomislav <tplese@gmail.com>
2020-09-02 22:24:51 +0200

    started serving site on /album

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const albumRouter = require('./src/routes/albumRoutes');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5100;
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'pug');
 
-app.use('/', albumRouter());
+app.use('/album', albumRouter());
 
 app.get('/', (req, res) => {
   res.render('layout');
--- src/controllers/albumController.js
+++ src/controllers/albumController.js
@@ -1,13 +1,83 @@
 //const SshClient = require('ssh2-sftp-client');
+const { MongoClient, ObjectID } = require('mongodb');
 const debug = require('debug')('app:albumController');
+const path = require('path');
+const fs = require('fs');
+
+// photo directory paths - fulls and thumbs
+const photosFullsDir = path.join(__dirname, '..', '..', 'public', 'photos', 'fulls');
+const photosThumbsDir = path.join(__dirname, '..', '..', 'public', 'photos', 'thumbs');
+
+let photosFullsList = [];
+let photosFullsObj = {};
+let fullPhotoPath = path.join(photosFullsDir, 'img01.jpg');
+let thumbPhotoPath = path.join(photosThumbsDir, 'img01.jpg');
+let client;
+
+async function getPhotosDbCollection() {
+  try {
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'albumshares';
+
+    client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    debug('getPhotosDbCollection -> Connected correctly to server');
+
+    const db = client.db(dbName);
+    const col = db.collection('photos');
+
+    return col;
+  } catch (err) {
+    debug(err.stack);
+  }
+
+  return 1;
+}
 
 module.exports = function albumController() {
   async function readPhotoDirectory(req, res, next) {
+    debug('readPhotoDirectory');
+    /*
+    try {
+      fs.readdir(photosFullsDir, (err, files) => {
+        if (err) {
+          throw err;
+        };
+        photosFullsList = files;
+      });
 
+      photosFullsList.forEach(photo => {
+        let noJpg = photo.slice(0, -4);
+        photosFullsObj[noJpg] = photo;
+      });
+
+      const photosCollection = await getPhotosDbCollection(client);
+      
+      debug(`ONE OBJ: ${photosFullsObj['img01']}`);
+
+      //const result = await photosCollection.insertOne(photosFullsObj);
+      
+    
+    } catch (err) {
+			debug(err.stack);
+		}
+
+    client.close();
+    */  
     next();
   }
 
   async function renderPage(req, res, next) {
+    try {
+      res.render(
+        'photo-gallery',
+        {
+          fullPhotoPath,
+          thumbPhotoPath,
+        },
+      ); 
+    } catch {
+      debug(err.stack);
+    }
     next();
   }
 
