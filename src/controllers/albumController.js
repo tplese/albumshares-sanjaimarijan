887c496162d04be90c03481ca76de4216c96ada2
tplese <tplese@gmail.com>
2020-10-12 17:33:56 +0200

    connecting to mongodb from app fails. trying with authSource

--- src/controllers/albumController.js
+++ src/controllers/albumController.js
@@ -16,15 +16,15 @@ let client;
 
 async function getPhotosDbCollection() {
   try {
-    const url = 'mongodb://tomtom:9hotHwAcEvoq1NWDtPgLY2MlKhHRLqdh3dH2csFS9oih4Z7L@10.156.0.3:27017/albumshares';
-    //const dbName = 'albumshares';
+    const url = 'mongodb://tomtom:9hotHwAcEvoq1NWDtPgLY2MlKhHRLqdh3dH2csFS9oih4Z7L@localhost:27017/?authSource=albumshares';
+    const dbName = 'albumshares';
 
     client = await MongoClient.connect(url, { useUnifiedTopology: true });
     debug('getPhotosDbCollection -> Connected correctly to server');
 
-    //const db = client.db(dbName);
-    //const col = db.collection('photos');
-    const col = client.collection('photos');
+    const db = client.db(dbName);
+    const col = db.collection('photos');
+    //const col = client.collection('photos');
 
     return col;
   } catch (err) {
